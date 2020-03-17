d821adf90b3c7073f0c2d54c7b25e7faa72318c0
Santyr <santi1703@gmail.com>
2020-03-17 16:12:40 -0300

    Resuelto lo del login? Vamos a averiguarlo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -39,13 +40,14 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function redirectTo()
+    protected function authenticated(Request $request, $user)
     {
-        $role = Auth::user()->role;
-        if($role == 0) {
+        if ($user->role == 0) {
             $this->redirectRoute = RouteServiceProvider::ADMIN_HOME;
         }
 
-        return $this->redirectRoute;
+        if (Auth::check()) {
+            return redirect($this->redirectRoute);
+        }
     }
 }
